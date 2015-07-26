e8c6f3fe384333fc8362f03119ae5a4d0e78942d
Mighten Dai <mighten.dai@gmail.com>
2015-07-26 21:28:31 +0800

    Removed show Pi in dialog, but added basic arithmetic calculating

--- x87_FPU_on_chip/x87_FPU_on_chip.asm
+++ x87_FPU_on_chip/x87_FPU_on_chip.asm
@@ -3,8 +3,8 @@
 ;
 ;   Demonstration the Usage of the x87 FPU on chip
 ;
-;	 Begin:  15:43, July 26, 2015, by Mighten Dai<mighten.dai@gmail.com>
-;	Latest:  19:59, July 26, 2015, by Mighten Dai<mighten.dai@gmail.com>
+;	 Begin:  20:00, July 26, 2015, by Mighten Dai<mighten.dai@gmail.com>
+;	Latest:  21:25, July 26, 2015, by Mighten Dai<mighten.dai@gmail.com>
 ;
 ;	********The required the following support(s):
 ;      1> Win32-API  support
@@ -23,39 +23,92 @@ includelib		kernel32.lib
 
 ;-----------------------------------------------------------------------------------
 		.data
-szBuffer		dd   0
-szText			db   100	dup(0)
-szCaption		db	"Demo: x87 FPU on chip", 0
-szFormat		db	"Pi = 0x%X in Floating-point Number", 0dh, 0
+;  All of the following number were calculated by MSVS2010
+f_pi			dd	?
+f3_250			dd	40500000h   ; floating-point number 3.250, respect to IEEE 754
+
+fadd_result		dd	?
+fsub_result		dd	?
+fmul_result		dd	?
+fdiv_result		dd	?
+
+szBuffer		db   64		dup(0)
 
 ;-----------------------------------------------------------------------------------
 		.code
 start:
 	finit
-	fldpi   ; push the PI's value into FPU-register Stack
-	lea		eax, szBuffer
-	fstp	dword ptr [eax]
-	mov		ecx, [eax]
 
-	; the prototype for wsprintfA
-	;      int wsprintfA(  LPTSTR lpOut, LPCTSTR lpFmt, ... );
-	push		ecx    ; The argument, only one
-	push		offset szFormat	; LPCTSTR lpFmt
-	push		offset szText	; LPTSTR lpOut
-	call		wsprintfA
-	add			esp, 000Ch    ; __cdecl Calling convention requires calling function clean the stack.
+	fld		dword ptr [f3_250]
+	fldpi   ; push Pi
+	fadd	ST, ST(1)       ;  Perform ST(0) = Pi + 3.250
+	fstp	dword ptr [fadd_result]  ; Store the value on ST(0) => 6.391593
+	fstp	dword ptr [szBuffer]   ; Balance the stack
 
-	;  Now display the target on screen by using MessageBox.
-	; Prototype for MessageBox
-	;    int MessageBox(  HWND hWnd, LPCTSTR lpText, LPCTSTR lpCaption, UINT uType );
-	push	MB_OK
-	push	offset szCaption
-	push	offset szText
-	push	NULL
-	call	MessageBox        ; MessageBox shows that Pi = 0100 0000 0100 1001 0000 1111 1101 1011
-                              ; Refer to as IEEE 754 Standard for Binary coded Floating-point
-							  ; is  0_10000000_10010010000111111011011
+	fld		dword ptr [f3_250]
+	fldpi   ; push Pi
+	fsub	ST(1), ST       ;  Perform ST(1) = 3.250 - Pi
+	fstp	dword ptr [szBuffer]   ; Balance the stack
+	fstp	dword ptr [fsub_result]  ; Store the value on ST(1)=> 0.1084073
+
+	fld		dword ptr [f3_250]
+	fldpi   ; push Pi
+	fsub	ST, ST(1) ;  Perform ST(1) = PI - 3.250
+	fstp	dword ptr [fsub_result]  ; Store the value on ST(1)=> -0.1084073
+	fstp	dword ptr [szBuffer]   ; Balance the stack
+
+	fld		dword ptr [f3_250]
+	fldpi   ; push Pi
+	fmul	ST, ST(1)       ;  Perform ST(0) = Pi * 3.250
+	fstp	dword ptr [fmul_result]  ; Store the value on ST(0) => 10.21018
+	fstp	dword ptr [szBuffer]   ; Balance the stack
+
+	fld		dword ptr [f3_250]
+	fldpi   ; push Pi
+	fdiv	ST(1), ST       ;  Perform ST(0) = 3.250 / Pi
+	fstp	dword ptr [szBuffer]     ; Balance the stack
+	fstp	dword ptr [fdiv_result]  ; Store the value on ST(0) => 1.034507
+
+	fld		dword ptr [f3_250]
+	fldpi   ; push Pi
+	fdiv	ST, ST(1)   ;  Perform ST(0) = Pi / 3.250
+	fstp	dword ptr [fdiv_result]  ; Store the value on ST(0) => 0.9666439
+	fstp	dword ptr [szBuffer]     ; Balance the stack
 
 	invoke  ExitProcess, 0 ; == return 0;
 
-end	start	
+end	start
+;
+; ------> Disassembled by OllyDebug V1.10
+;
+; 00401001  |. DBE3           FINIT
+; 00401003  |. D905 04304000  FLD DWORD PTR DS:[403004]
+; 00401009  |. D9EB           FLDPI
+; 0040100B  |. D8C1           FADD ST,ST(1)
+; 0040100D  |. D91D 08304000  FSTP DWORD PTR DS:[403008]
+; 00401013  |. D91D 18304000  FSTP DWORD PTR DS:[403018]
+; 00401019  |. D905 04304000  FLD DWORD PTR DS:[403004]
+; 0040101F  |. D9EB           FLDPI
+; 00401021  |. DCE9           FSUB ST(1),ST
+; 00401023  |. D91D 18304000  FSTP DWORD PTR DS:[403018]
+; 00401029  |. D91D 0C304000  FSTP DWORD PTR DS:[40300C]
+; 0040102F  |. D905 04304000  FLD DWORD PTR DS:[403004]
+; 00401035  |. D9EB           FLDPI
+; 00401037  |. D8E1           FSUB ST,ST(1)
+; 00401039  |. D91D 0C304000  FSTP DWORD PTR DS:[40300C]
+; 0040103F  |. D91D 18304000  FSTP DWORD PTR DS:[403018]
+; 00401045  |. D905 04304000  FLD DWORD PTR DS:[403004]
+; 0040104B  |. D9EB           FLDPI
+; 0040104D  |. D8C9           FMUL ST,ST(1)
+; 0040104F  |. D91D 10304000  FSTP DWORD PTR DS:[403010]
+; 00401055  |. D91D 18304000  FSTP DWORD PTR DS:[403018]
+; 0040105B  |. D905 04304000  FLD DWORD PTR DS:[403004]
+; 00401061  |. D9EB           FLDPI
+; 00401063  |. DCF9           FDIV ST(1),ST
+; 00401065  |. D91D 18304000  FSTP DWORD PTR DS:[403018]
+; 0040106B  |. D91D 14304000  FSTP DWORD PTR DS:[403014]
+; 00401071  |. D905 04304000  FLD DWORD PTR DS:[403004]
+; 00401077  |. D9EB           FLDPI
+; 00401079  |. D8F1           FDIV ST,ST(1)
+; 0040107B  |. D91D 14304000  FSTP DWORD PTR DS:[403014]
+; 00401081  |. D91D 18304000  FSTP DWORD PTR DS:[403018]
