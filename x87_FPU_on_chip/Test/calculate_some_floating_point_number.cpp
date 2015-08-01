72c8d7a6f203997b4dbe3e725ab9e01763c551c9
Mighten Dai <mighten.dai@gmail.com>
2015-08-01 19:16:30 +0800

    test file by using C++

---
+++ x87_FPU_on_chip/Test/calculate_some_floating_point_number.cpp
@@ -0,0 +1,20 @@
+//
+//     calculate_some_floating_point_number.cpp
+//
+//		To form a convenient way to program more effinciently
+//
+//     Avoid committing mistakes.
+//      Begin: 20:18, July 26, 2015, by Mighten Dai<mighten.dai@gmail.com>
+//     Latest:                          Mighten Dai<mighten.dai@gmail.com>
+//
+#include <iostream>
+
+using namespace std;
+
+int main(void )
+{
+	//Now calculating some floating-point numbers
+	float  f3_250 = 3.250f;  //00 00 50 40(Little-Endian Binary) = 40500000h
+
+	return 0;
+}
